2d303d14884e264b3519db3fea1b1234e41d6364
jiangxy9 <jiangxy9@lenovo.com>
2019-11-12 15:25:52 +0800

    job config

--- src/main/java/com/jiang/demo/elasticjob/config/ElasticJobConfig.java
+++ src/main/java/com/jiang/demo/elasticjob/config/ElasticJobConfig.java
@@ -11,7 +11,6 @@ import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import com.jiang.demo.elasticjob.job.SimpleJobDemo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
